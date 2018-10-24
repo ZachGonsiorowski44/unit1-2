c78923fbee0e7a4f71baedf15bd8ded3345feb74
Zach Gonsiorowski <zachary.gonsiorowski@student.rjuhsd.us>
2018-10-24 20:28:40 +0000

    Unit-1

---
+++ TimeStamp.py
@@ -0,0 +1,9 @@
+a = int(input()) * 60 * 60
+b = int(input()) * 60
+c = int(input())
+
+d = int(input()) * 60 * 60
+e = int(input()) * 60
+f = int(input())
+
+print((d+e+f) - (a+b+c))
---
+++ Unit 1/AreaRT.py
@@ -0,0 +1,6 @@
+# Read the numbers b and h like this:
+a = int(input())
+b = int(input())
+
+# Print the result with print()
+print(0.5 * a * b)
